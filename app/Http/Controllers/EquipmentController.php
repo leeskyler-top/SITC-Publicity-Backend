8cddbabf83da4559c90dba67cae7ac5283d80b3b
leeskyler-top <3188045173@qq.com>
2023-06-15 21:04:10 +0800

    feat: Add Batch Add Equipments (2023-06-15 21:04)

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Equipment;
+use App\Models\EquipmentAudit;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class EquipmentController extends Controller
 {
@@ -130,6 +134,76 @@ class EquipmentController extends Controller
             return $this->jsonRes(200, null, "设备删除成功");
         }
     }
+
+    public function batchStore(Request $request)
+    {
+        $data = $request->only(['csv_file']);
+        $validator = Validator::make($data, [
+            'csv_file' => 'required|mimes:csv,txt'
+        ]);
+        if ($validator->fails()) {
+            return $this->jsonRes(422, '必须是csv文件');
+        }
+        $file = $request->file('csv_file')->openFile('r');
+        $file->setFlags(\SplFileObject::READ_CSV);
+
+        $validationRules = [
+            'fixed_assets_num' => 'nullable|string',
+            'name' => 'nullable|string',
+            'model' => 'nullable|string',
+            'status' => 'nullable|in:damaged,unassigned',
+            'create_time' => 'nullable|date_format:Y-m-d H:i:s'
+        ];
+
+        DB::beginTransaction();
+
+        try {
+            foreach ($file as $key => $element) {
+                if ($key === 0) { // Skip the header row
+                    continue;
+                }
+                if (count($element) !== 5) {
+                    throw new \Exception("Invalid CSV format. Please check the file contents.");
+                }
+                $validator = Validator::make([
+                    'fixed_assets_num' => $element[0],
+                    'name' => $element[1],
+                    'model' => $element[2],
+                    'status' => $element[3],
+                    'create_time' => $element[4]
+                ], $validationRules,  [
+                    'fixed_assets_num' => '固定资产编号必须为字符串',
+                    'name' => '设备名称必须为字符串',
+                    'model' => '模型必须为字符串',
+                    'status' => '状态必须为空闲或受损',
+                    'create_time' => '入库时间必须为时间'
+                ]);
+                if ($validator->fails()) {
+                    DB::rollBack();
+                    return $this->jsonRes(400, $validator->errors()->first());
+                }
+                $equipment = Equipment::create([
+                    'is_admin' => $element[0],
+                    'uid' => $element[1],
+                    'name' => $element[2],
+                    'email' => $element[3],
+                    'password' => $element[4],
+                    'department' => $element[5],
+                    'classname' => $element[6],
+                    'note' => $element[7],
+                ]);
+                $equipment->refresh();
+                $equipments[] = $equipment;
+            }
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['msg' => '在执行批量操作时发生严重错误，已回滚操作！'],500);
+        }
+
+        return $this->jsonRes(200, '设备批量添加完成', $equipments);
+    }
+
 
     // 我的设备
     public function showMyEquipment($type)
