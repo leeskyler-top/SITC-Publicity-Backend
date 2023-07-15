cdca985c012b3ec9db0b9a8e960b398f0fd9340c
leeskyler-top <3188045173@qq.com>
2023-07-15 14:01:10 +0800

    fixed: Removed Welcome View (2023-07-15 14:01)

--- routes/api.php
+++ routes/api.php
@@ -36,8 +36,8 @@ Route::middleware("auth:api")->group(function () {
         Route::get("/unassigned", [EquipmentController::class, 'indexUnassignedEquipments']);
         Route::post("/apply", [EquipmentController::class, 'equipmentApply']);
         Route::get("/my/{status}", [EquipmentController::class, 'showMyEquipment']);
-        Route::post("/back/{equipment_id}", [EquipmentController::class, 'back']);
-        Route::post("/report/{equipment_id}", [EquipmentController::class, 'reportEquipment']);
+        Route::post("/back/{equipment_rent_application_id}", [EquipmentController::class, 'back']);
+        Route::post("/report/{equipment_rent_application_id}", [EquipmentController::class, 'reportEquipment']);
         Route::post("/delay-apply/{equipment_rent_application_id}", [EquipmentController::class, 'delayApply']);
     });
 });
