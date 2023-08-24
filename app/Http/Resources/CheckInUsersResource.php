41426ce0f7809c8aaaf276a82d8c4427de04c151
leeskyler-top <3188045173@qq.com>
2023-08-24 23:00:53 +0800

    fixed: Add Some Info in CIUR (2023-08-24 23:00)

--- app/Http/Resources/CheckInUsersResource.php
+++ app/Http/Resources/CheckInUsersResource.php
@@ -16,8 +16,11 @@ class CheckInUsersResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'title' => $this->checkIn->title,
             'activity_title' => $this->checkIn->activity->title,
             'activity_place' => $this->checkIn->activity->place,
+            'start_time' => $this->checkIn->start_time,
+            'end_time' => $this->checkIn->end_time,
             'checkin_status' => $this->checkIn->status,
             'status' => $this->status
         ];
